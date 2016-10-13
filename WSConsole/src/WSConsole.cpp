f7f14a2c85d1d6719721e74b8d148ddf366281d9
TaikaRuis <ville.ehc@gmail.com>
2016-10-13 22:20:14 +0300

    Test commit

--- WSConsole/src/WSConsole.cpp
+++ WSConsole/src/WSConsole.cpp
@@ -10,6 +10,6 @@
 using namespace std;
 
 int main() {
-	cout << "!!!Hello World päivitetty!!!" << endl; // prints !!!Hello World!!!
+	cout << "!!!Hello World päivitetty 2x!!!" << endl; // prints !!!Hello World!!!
 	return 0;
 }
