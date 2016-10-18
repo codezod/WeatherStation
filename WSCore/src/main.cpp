7b80ab2f4ed07c88063bb5051b5e5ca12a4514a9
juhani <juhani@vanhalat.net>
2016-10-18 21:09:55 +0300

    Raspberry Pi settings to project config

---
+++ WSCore/src/main.cpp
@@ -0,0 +1,15 @@
+//============================================================================
+// Name        : main.cpp
+// Author      : Juhani Vanhala
+// Version     :
+// Copyright   : Your copyright notice
+// Description : entry point for WSCore
+//============================================================================
+
+#include <iostream>
+using namespace std;
+
+int main() {
+	cout << "Weather Station core service starting" << endl; 
+	return 0;
+}
