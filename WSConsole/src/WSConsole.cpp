191b844576960d558fd5660fd903eb80c8a32df4
juhani <juhani@vanhalat.net>
2016-10-20 23:10:58 +0300

    File comments updated

--- WSConsole/src/WSConsole.cpp
+++ WSConsole/src/WSConsole.cpp
@@ -1,10 +1,23 @@
-//============================================================================
-// Name        : WSConsole.cpp
-// Author      : Juhani
-// Version     :
-// Copyright   : Your copyright notice
-// Description : Hello World in C++, Ansi-style
-//============================================================================
+/* Copyright 2016 Juhani Vanhala. All Rights Reserved.
+
+Licensed under The MIT License (MIT)
+
+You may not use this file except in compliance with the License.
+You may obtain a copy of the License at https://opensource.org/licenses/mit-license.php
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED,
+INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR
+A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT
+HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF
+CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE
+OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+============================================================================
+ Name        : WSConsole.cpp
+ Author      :
+ Version     : 0.1
+ Description : entry point for WSConsole
+============================================================================
+*/
 
 #include <iostream>
 using namespace std;
